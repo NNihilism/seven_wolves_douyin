fe5e08c9f46c885769bccf9c633fbaf3ffbaefcb
NNihilism <405307400@qq.com>
2023-02-02 17:37:05 +0800

    add consts.go and errno.go

---
+++ pkg/consts/consts.go
@@ -0,0 +1,12 @@
+package consts
+
+const (
+	// server name
+	UserServiceName = "user"
+	ApiServiceName  = "api"
+
+	ExportEndpoint = ":4317"
+
+	// ECTD server register and find
+	ETCDAddress = "127.0.0.1:2379"
+)

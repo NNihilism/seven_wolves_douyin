62b0d073a95a748b28bd344e9e8c61c032d2c6d6
NNihilism <405307400@qq.com>
2023-02-03 20:28:19 +0800

    add filed name in hadler.go

--- cmd/user/handler.go
+++ cmd/user/handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	user "douyin/kitex_gen/user"
+	"time"
 )
 
 // UserServiceImpl implements the last service interface defined in the IDL.
@@ -11,7 +12,20 @@ type UserServiceImpl struct{}
 // CreateUser implements the UserServiceImpl interface.
 func (s *UserServiceImpl) CreateUser(ctx context.Context, req *user.CreateUserRequest) (resp *user.CreateUserResponse, err error) {
 	// TODO: Your code here...
-	return
+	return &user.CreateUserResponse{
+		BaseResp: &user.BaseResp{
+			StatusCode:    0,
+			StatusMessage: "Yeah, created successfully",
+			ServiceTime:   time.Now().Unix(),
+		},
+		User: &user.User{
+			Id:            8888,
+			Username:      "Jack",
+			Password:      "123456",
+			FollowCount:   80,
+			FollowerCount: 100,
+		},
+	}, nil
 }
 
 // MGetUser implements the UserServiceImpl interface.

9ee7c1dac629c9eb2b73f279e8fbfbb1337b2790
NNihilism <405307400@qq.com>
2023-02-21 10:12:06 +0800

    Add examle : get userid from token

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -4,13 +4,15 @@ package main
 
 import (
 	handler "douyin/cmd/api/biz/handler"
+	"douyin/cmd/api/biz/mw"
 
 	"github.com/cloudwego/hertz/pkg/app/server"
 )
 
 // customizeRegister registers customize routers.
 func customizedRegister(r *server.Hertz) {
-	r.GET("/ping", handler.Ping)
+	auth := r.Group("/auth", mw.JwtMiddleware.MiddlewareFunc())
+	auth.GET("/ping", handler.Ping)
 
 	// your code ...
 }

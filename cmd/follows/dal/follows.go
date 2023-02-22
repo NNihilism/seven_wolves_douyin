d5e3a6105205a5b3e27cd6862612d0ca7b27f187
ZFT <959906028@qq.com>
2023-02-22 16:53:36 +0800

    2/22

--- cmd/follows/dal/follows.go
+++ cmd/follows/dal/follows.go
@@ -22,14 +22,10 @@ func (f *Follow) TableName() string {
 // 关注/取关操作
 func UpdateFollowStatus(ctx context.Context, req *follows.RelationActionRequest) (resp *follows.RelationActionResponse, err error) {
 	resp = new(follows.RelationActionResponse)
-	// resp.BaseResp = BaseResp.NewBaseResp()
 	actionType, _ := strconv.Atoi(req.ActionType)
 	if actionType == 1 {
 		var count int64
 		DB.Model(&Follow{}).Where("user_id = ? and follower_id = ?", req.UserId, req.ToUserId).Count(&count)
-		fmt.Println(count)
-		fmt.Println(req.UserId)
-		fmt.Println(req.ToUserId)
 		if count == 0 {
 			follow := Follow{UserId: int(req.UserId), FollowerId: int(req.ToUserId)}
 			if err = DB.Create(&follow).Error; err != nil {
@@ -45,7 +41,7 @@ func UpdateFollowStatus(ctx context.Context, req *follows.RelationActionRequest)
 			}
 		}
 	} else {
-		if err = DB.Where("userid = ? and followerid = ?", int(req.UserId), int(req.ToUserId)).Delete(Follow{}).Error; err != nil {
+		if err = DB.Where("user_id = ? and follower_id = ?", int(req.UserId), int(req.ToUserId)).Delete(Follow{}).Error; err != nil {
 			fmt.Println("取关失败", err)
 		} else {
 			baseResp := &follows.BaseResp{
@@ -71,11 +67,9 @@ func GetFollowList(ctx context.Context, req *follows.GetFollowListRequest) (resp
 	userId := req.UserId
 	users := make([]*follows.User, 0)
 	followsId := make([]int, 0)
-	fmt.Println("------------------------------")
 	DB.Model(&Follow{}).Select("follower_id").Where("user_id = ?", userId).Scan(&followsId)
 	fmt.Println(followsId)
 	DB.Table("user").Where("id in ?", followsId).Find(&users)
-
 	resp.BaseResp = &follows.BaseResp{
 		StatusCode:    0,
 		StatusMessage: "查询成功",
@@ -91,7 +85,6 @@ func GetFollowerList(ctx context.Context, req *follows.GetFollowerListRequest) (
 	followersId := make([]int, 0)
 	DB.Model(&Follow{}).Select("user_id").Where("follower_id = ?", userId).Scan(&followersId)
 	users := make([]*follows.User, 0)
-	fmt.Println(followersId)
 	DB.Table("user").Where("id in ?", followersId).Find(&users)
 	resp.BaseResp = &follows.BaseResp{
 		StatusCode:    0,
